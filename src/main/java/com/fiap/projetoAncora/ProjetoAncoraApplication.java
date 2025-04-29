1cb8414b1307b183a97e769a0d2b8c57605a4f47
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-29 09:06:02 -0300

    meu_primeiro_commit_spring

---
+++ src/main/java/com/fiap/projetoAncora/ProjetoAncoraApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.projetoAncora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAncoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAncoraApplication.class, args);
+	}
+
+}
